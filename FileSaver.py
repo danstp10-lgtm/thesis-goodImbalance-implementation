1daf3c0b2101a6cca7529d485b6b0117a155d6ce
daniel <danstp10@gmail.com>
2026-07-27 10:43:12 +0200

    added comments

--- FileSaver.py
+++ FileSaver.py
@@ -12,6 +12,7 @@ class FileSaver:
         self.frame_counter = 0
         self._running = True
 
+        # start writing loop in a seperate thread
         self.worker_thread = threading.Thread(
             target=self._writer_loop, daemon=True
         )
@@ -19,18 +20,20 @@ class FileSaver:
         self.worker_thread.start()
 
     def save(self,frame,timecode):
+        # add frames to queue from data processing script
         self.queue.put((self.frame_counter, timecode, frame.copy()))
         self.frame_counter += 1
 
     def _writer_loop(self):
-        while self._running and not self.queue.empty():
+        # write to output_dir while thread is running or there are unsaved frames
+        while self._running or not self.queue.empty():
             try:
-                frame_id, timecode, frame = self.queue.get(timeout=0.1)
-                filename = (
+                frame_id, timecode, frame = self.queue.get(timeout=0.1) # get data from queue
+                filename = ( # construct filename
                     self.output_dir
                     / f"frame_{frame_id:06d}_tc_{int(timecode)}.npz"
                 )
-
+                # save to file
                 np.savez_compressed(
                     filename = filename,
                     frame = frame,
@@ -38,7 +41,7 @@ class FileSaver:
                     frame_id = frame_id,
                 )
 
-                self.queue.task_done()
+                self.queue.task_done() # announce task done to thread, ensures all items put in the queue get processed
             except queue.Empty:
                 continue
     
