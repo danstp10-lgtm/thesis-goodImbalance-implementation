b379d644bca027f4077f63835457ca7c319cecee
daniel <danstp10@gmail.com>
2026-08-10 11:59:51 +0200

    converted display frame into a colored image and moved raw TSP data extraction

--- determineBalance.py
+++ determineBalance.py
@@ -27,7 +27,11 @@ if __name__ == "__main__":
     cached_raw_frame = np.zeros(
         (rows, columns * 2 + between_patch_distance), dtype=np.uint8
     )
-    display_frame = np.zeros(cached_raw_frame.shape, np.uint8)
+    # display_frame = np.zeros(cached_raw_frame.shape, np.uint8)
+    display_frame = cv2.cvtColor(cached_raw_frame, cv2.COLOR_GRAY2BGR)
+    COLOR_COM = (0, 255, 0)  # Bright Green
+    COLOR_XCOM = (0, 255, 255)  # Bright Yellow
+    COLOR_HAND = (255, 255, 255)  # Bright Yellow
 
     print("Starting sync between Touch Sense Patch 1|2 - 7Hz and Xsens MVN Software - 240Hz")
 
@@ -72,6 +76,11 @@ if __name__ == "__main__":
                     drift_error = body_tracker_coords - xsens2vive_segments
                     t += ALPHA * drift_error
                     if TSP_L.frame_available and TSP_R.frame_available:
+                        # Raw TSP data
+                        raw_frame_L, raw_frame_R = get_raw_frames(TSP_L,TSP_R)
+                        display_frame = np.zeros(cached_raw_frame.shape, np.uint8)                   
+                        padding = np.zeros((rows,between_patch_distance)) # add empty space between hands
+                        cached_raw_frame = np.concatenate([raw_frame_L, padding,raw_frame_R], axis=1).astype(np.uint8) 
 
                         # Calculate XCoM, with smoothing
                         oldest_time, oldest_com = com_history[0]
@@ -88,7 +97,7 @@ if __name__ == "__main__":
                             xsens_XCoM = xsens_CoM.copy()
                         # print(f"time period: {time_sec}:{oldest_time}={dt} | velocity_CoM: {velocity_CoM} | XCoM: {xsens_XCoM}")
                         # print(len(com_history))
-                        com_history.clear()                  
+                        com_history.clear() # clear history for next timestep                  
 
                         # Transform Xsens to TSP data
                         TSP_XCoM = [0,0]
@@ -105,14 +114,16 @@ if __name__ == "__main__":
                         # print(f"dist x {distance_cm[0]} | dist y {distance_cm[1]} | dist z {distance_cm[2]}")
                         print(f"{xsens2TSP_segments_pixel}")
                         if 0 < xsens2TSP_segments_pixel[0] < display_frame.shape[0] and 0 < xsens2TSP_segments_pixel[1] < display_frame.shape[1]:
-                            display_frame[xsens2TSP_segments_pixel[0]][xsens2TSP_segments_pixel[1]]= 255
+                            # display_frame[xsens2TSP_segments_pixel[0]][xsens2TSP_segments_pixel[1]]= 255
+                            cv2.drawMarker(
+                                displayFrame,
+                                (xsens2TSP_segments_pixel[0], xsens2TSP_segments_pixel[1]),
+                                color=COLOR_HAND,
+                                markerType=cv2.MARKER_CROSS,
+                                markerSize=3,
+                                thickness=1,
+                            )
                         
-                        # Raw TSP data
-                        raw_frame_L, raw_frame_R = get_raw_frames(TSP_L,TSP_R)
-                        display_frame = np.zeros(cached_raw_frame.shape, np.uint8)                   
-                        padding = np.zeros((rows,between_patch_distance)) # add empty space between hands
-                        cached_raw_frame = np.concatenate([raw_frame_L, padding,raw_frame_R], axis=1).astype(np.uint8) 
-
                         # Calculate TSP metrics
                         CoP_cm = calculate_CoP(cached_raw_frame, display_frame)
                         BoS_cm = calculate_BoS(cached_raw_frame, display_frame)
@@ -135,16 +146,23 @@ if __name__ == "__main__":
                             if len(distances_CoP2BoS) > 0:
                                 min_dist_CoP2BoS = np.min(distances_CoP2BoS)
                                 min_dist_XCoM2BoS = np.min(distances_XCoM2BoS)
-                                saver.save_metrics(time_sec, CoP_cm, min_dist_CoP2BoS, TSP_XCoM, min_dist_XCoM2BoS)
+                                # saver.save_metrics(time_sec, CoP_cm, min_dist_CoP2BoS, TSP_XCoM, min_dist_XCoM2BoS)
                         
                         # Save data
-                        saver.save_frame(cached_raw_frame, time_sec)
-                        saver.increment_frame_count()
+                        # saver.save_frame(cached_raw_frame, time_sec)
+                        # saver.increment_frame_count()
 
                         # Show XCoM pixel on display
                         # XCoM_pixel = [round(TSP_XCoM[0]/0.8),round(TSP_XCoM[1]/0.6)]
                         # if 0 < XCoM_pixel[0] < display_frame.shape[0] and 0 < XCoM_pixel[1] < display_frame.shape[1]:
-                        #     display_frame[XCoM_pixel[0]][XCoM_pixel[1]] = 255
+                        #     cv2.drawMarker(
+                        #         displayFrame,
+                        #         (XCoM_pixel[0],XCoM_pixel[1]),
+                        #         color=COLOR_XCOM,
+                        #         markerType=cv2.MARKER_STAR,
+                        #         markerSize=2,
+                        #         thickness=1,
+                        #     )
                         #     print("XCoM in bounds")
                         # else:
                         #     print("XCoM out of bounds")
