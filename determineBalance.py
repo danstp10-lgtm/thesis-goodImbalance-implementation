daeb29f751debd0506c39f98136959db9a807a6e
daniel <danstp10@gmail.com>
2026-08-11 10:30:30 +0200

    made TSP_utlis for general utils and moved XCoM and minimum distance calculation to it

--- determineBalance.py
+++ determineBalance.py
@@ -1,5 +1,5 @@
 from XsensUDPListener import XsensUDPListener 
-from TSP_utils import *
+from utils import *
 from FileSaver import FileSaver
 import time
 from support_functions import *
@@ -35,13 +35,16 @@ if __name__ == "__main__":
     print("Starting sync between Touch Sense Patch 1|2 - 7Hz and Xsens MVN Software - 240Hz")
 
     xsens_XCoM = None
-    omega_0 = np.sqrt(9.81/0.6)
     com_history = deque()
+
+    # Calibration parameters
     calibrated = False
     calibration_samples = 300
     vive_samples = []
     xsens_samples = []
     ALPHA = 0.01 
+
+    # Vive Y-up to Z-up matrix
     M_swap = np.array([
         [0, 0, 1],  # Grid X gets Tracker Z
         [1, 0, 0],  # Grid Y gets Tracker X
@@ -73,9 +76,10 @@ if __name__ == "__main__":
                     # Compensate for drift overtime by applying R,t and comparing to actual tracker on the body 
                     xsens2vive_segments = ((R @ xsens_segments[0]) + t)        
                     drift_error = body_tracker_coords - xsens2vive_segments
-                    # print(f"drift error:{drift_error}")
                     t += ALPHA * drift_error
-                    if TSP_L.frame_available and TSP_R.frame_available:
+                    # print(f"drift error:{drift_error}")
+
+                    if TSP_L.frame_available and TSP_R.frame_available: # on TSP available frame compute variables
                         # Raw TSP data
                         raw_frame_L, raw_frame_R = get_raw_frames(TSP_L,TSP_R)
                         display_frame = cv2.cvtColor(cached_raw_frame, cv2.COLOR_GRAY2BGR)                  
@@ -83,19 +87,7 @@ if __name__ == "__main__":
                         cached_raw_frame = np.concatenate([raw_frame_L, padding,raw_frame_R], axis=1).astype(np.uint8) 
 
                         # Calculate XCoM, with smoothing
-                        oldest_time, oldest_com = com_history[0]
-                        dt = time_sec - oldest_time
-                        if len(com_history) > 2:
-                            times = np.array([t for t, _ in com_history])
-                            positions = np.array([pos for _, pos in com_history])
-                            t_centered = times - times[0]
-                            velocity_CoM, _ = np.polyfit(t_centered, positions, deg=1)
-                            xsens_XCoM = xsens_CoM + (velocity_CoM / omega_0)
-                        else:
-                            velocity_CoM = np.zeros(2)
-                            xsens_XCoM = xsens_CoM.copy()
-                        # print(f"time period: {time_sec}:{oldest_time}={dt} | velocity_CoM: {velocity_CoM} | XCoM: {xsens_XCoM}")
-                        # print(len(com_history))
+                        xsens_XCoM = calculate_XCoM(com_history, xsens_CoM, time_sec)
                         com_history.clear() # clear history for next timestep                  
 
                         # Transform Xsens to TSP data
@@ -106,46 +98,30 @@ if __name__ == "__main__":
                         TSP_XCoM = transform_Xsens2TSP(xsens_XCoM, R, t, t_TSP, R_TSP ) * 100 # transform to TSP
                         
                         # Check transformation with left hand
-                        xsens2TSP_segments = transform_Xsens2TSP(xsens_segments[0],R, t, t_TSP, R_TSP) * 100
-                        xsens2TSP_segments_pixel = [int(np.round(xsens2TSP_segments[0]/0.8)),int(np.round(xsens2TSP_segments[1]/0.6))] 
-                        # print(f"hand coords {xsens2TSP_segments_pixel} in shape{display_frame.shape}")
-                        if 0 < xsens2TSP_segments_pixel[0] < display_frame.shape[1] and 0 < xsens2TSP_segments_pixel[1] < display_frame.shape[0]:
-                            # display_frame[xsens2TSP_segments_pixel[0]][xsens2TSP_segments_pixel[1]]= 255
-                            cv2.drawMarker(
-                                display_frame,
-                                (xsens2TSP_segments_pixel[0], xsens2TSP_segments_pixel[1]),
-                                color=COLOR_HAND,
-                                markerType=cv2.MARKER_CROSS,
-                                markerSize=3,
-                                thickness=1,
-                            )
+                        # xsens2TSP_segments = transform_Xsens2TSP(xsens_segments[0],R, t, t_TSP, R_TSP) * 100
+                        # xsens2TSP_segments_pixel = [int(np.round(xsens2TSP_segments[0]/0.8)),int(np.round(xsens2TSP_segments[1]/0.6))] 
+                        # # print(f"hand coords {xsens2TSP_segments_pixel} in shape{display_frame.shape}")
+                        # if 0 < xsens2TSP_segments_pixel[0] < display_frame.shape[1] and 0 < xsens2TSP_segments_pixel[1] < display_frame.shape[0]:
+                        #     # display_frame[xsens2TSP_segments_pixel[0]][xsens2TSP_segments_pixel[1]]= 255
+                        #     cv2.drawMarker(
+                        #         display_frame,
+                        #         (xsens2TSP_segments_pixel[0], xsens2TSP_segments_pixel[1]),
+                        #         color=COLOR_HAND,
+                        #         markerType=cv2.MARKER_CROSS,
+                        #         markerSize=3,
+                        #         thickness=1,
+                        #     )
                         
                         # Calculate TSP metrics
                         CoP_cm = calculate_CoP(cached_raw_frame, display_frame)
                         BoS_cm = calculate_BoS(cached_raw_frame, display_frame)
                         
                         # Calculate minimum distance of CoP and XCoM to BoS boundaries in cm, margin of stability b
-                        distances_CoP2BoS = []
-                        distances_XCoM2BoS = []
-                        if BoS_cm: # if there are any contours
-                            for i in range(len(BoS_cm)):
-                                j=i+1
-                                if j>=len(BoS_cm):
-                                    j=0
-                                x_1, x_2 = BoS_cm[i][0],BoS_cm[j][0]
-                                y_1, y_2 = BoS_cm[i][1],BoS_cm[j][1]
-                                A = y_2 - y_1
-                                B = x_1 - x_2
-                                C = x_2*y_1-x_1*y_2
-                                distances_CoP2BoS.append(np.abs(A*CoP_cm[0]+B*CoP_cm[1]+C)/(np.sqrt(np.power(A,2)+np.power(B,2))))
-                                distances_XCoM2BoS.append(np.abs(A*TSP_XCoM[0]+B*TSP_XCoM[1]+C)/(np.sqrt(np.power(A,2)+np.power(B,2))))
-                            if len(distances_CoP2BoS) > 0:
-                                min_dist_CoP2BoS = np.min(distances_CoP2BoS)
-                                min_dist_XCoM2BoS = np.min(distances_XCoM2BoS)
-                                saver.save_metrics(time_sec, CoP_cm, min_dist_CoP2BoS, TSP_XCoM, min_dist_XCoM2BoS)
-                        
+                        min_dist_CoP2BoS, min_dist_XCoM2BoS = calculate_min_dist(BoS_cm, CoP_cm, TSP_XCoM)
+
                         # Save data
                         saver.save_frame(cached_raw_frame, time_sec)
+                        saver.save_metrics(time_sec, CoP_cm, min_dist_CoP2BoS, TSP_XCoM, min_dist_XCoM2BoS)
                         saver.increment_frame_count()
 
                         # Show XCoM pixel on display
@@ -174,13 +150,7 @@ if __name__ == "__main__":
                 (3 * 224, 2 * 224),
                 interpolation=cv2.INTER_NEAREST,
             )
-            # raw_resized = cv2.resize(
-            #     cached_raw_frame,
-            #     (3 * 224, 2 * 224),
-            #     interpolation=cv2.INTER_NEAREST,
-            # )
             cv2.imshow("Synchronized Display", display_resized)
-            # cv2.imshow("Raw frame", raw_resized)
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
     finally:
--- utils.py
+++ utils.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 
-import serial
 import cv2
 from cv2 import drawContours
 import numpy as np
-import skimage
 from scipy.ndimage import binary_erosion
 from scipy.ndimage import median_filter
 from scipy.spatial import ConvexHull
@@ -14,6 +12,8 @@ from support_functions import *
 # # spacing 8,6mm
 pixel_X_length=0.8
 pixel_Y_length=0.6
+
+omega_0 = np.sqrt(9.81/0.6)
 
 COLOR_COP = (0, 0, 255)  # Bright Red
 COLOR_BOS = (255, 0, 0)  # Bright Blue
@@ -54,6 +54,44 @@ def calculate_BoS(raw_frame, display_frame):
         cv2.drawContours(display_frame, [BoS_pixel], -1, COLOR_BOS, 1)
         # cv2.polylines(display_frame, [BoS_pixel], isClosed=True, color=(0, 255, 255), thickness=1)
     return BoS_cm
+
+def calculate_XCoM(com_history, latest_CoM, time_sec):
+    oldest_time, oldest_com = com_history[0]
+    dt = time_sec - oldest_time
+    if len(com_history) > 2:
+        times = np.array([t for t, _ in com_history])
+        positions = np.array([pos for _, pos in com_history])
+        t_centered = times - times[0]
+        velocity_CoM, _ = np.polyfit(t_centered, positions, deg=1)
+        XCoM = latest_CoM + (velocity_CoM / omega_0)
+    else:
+        velocity_CoM = np.zeros(2)
+        XCoM = latest_CoM.copy()
+    # print(f"time period: {time_sec}:{oldest_time}={dt} | velocity_CoM: {velocity_CoM} | XCoM: {XCoM}")
+    # print(len(com_history))
+    return XCoM
+
+def calculate_min_dist(BoS, CoP,XCoM):
+    distances_CoP2BoS = []
+    distances_XCoM2BoS = []
+    min_dist_CoP2BoS = None
+    min_dist_XCoM2BoS = None
+    if BoS: # if there are any contours
+        for i in range(len(BoS)):
+            j=i+1
+            if j>=len(BoS):
+                j=0
+            x_1, x_2 = BoS[i][0],BoS[j][0]
+            y_1, y_2 = BoS[i][1],BoS[j][1]
+            A = y_2 - y_1
+            B = x_1 - x_2
+            C = x_2*y_1-x_1*y_2
+            distances_CoP2BoS.append(np.abs(A*CoP[0]+B*CoP[1]+C)/(np.sqrt(np.power(A,2)+np.power(B,2))))
+            distances_XCoM2BoS.append(np.abs(A*XCoM[0]+B*XCoM[1]+C)/(np.sqrt(np.power(A,2)+np.power(B,2))))
+        if len(distances_CoP2BoS) > 0 and len(distances_XCoM2BoS) > 0:
+            min_dist_CoP2BoS = np.min(distances_CoP2BoS)
+            min_dist_XCoM2BoS = np.min(distances_XCoM2BoS)
+    return min_dist_CoP2BoS, min_dist_XCoM2BoS
 
 def main():
     rows, columns, between_hand_distance = 27, 19, 15
