cdb5bbc640c966baddbe6a06533f59500d55ebaf
daniel <danstp10@gmail.com>
2026-08-13 17:01:42 +0200

    added Case C to plot

--- plotDistances.py
+++ plotDistances.py
@@ -4,18 +4,17 @@ from scipy.signal import savgol_filter  # Optional: for Savitzky-Golay filtering
 
 file_path_1 = 'recordings\session_20260813_135446\session_metrics.csv'
 file_path_2 = 'recordings\session_20260813_141016\session_metrics.csv'
+file_path_3 = "recordings\session_20260813_163839\session_metrics.csv"
 df = pd.read_csv(file_path_1)
 df.columns = df.columns.str.strip()
 
 # start time at 0
 df['timecode'] = df['timecode'] - df['timecode'].iloc[0]
-
 plt.figure(figsize=(12, 6), dpi=100)
-
 clean_cop = df['cop2bos_dist_cm'].interpolate(method='linear').bfill().ffill()
 clean_xcom = df['xcom2bos_dist_cm'].interpolate(method='linear').bfill().ffill()
 
-# 2. Apply Savitzky-Golay (window_length must be an odd integer)
+# Smoothing
 df['cop2bos_smooth'] = savgol_filter(clean_cop, window_length=11, polyorder=2)
 df['xcom2bos_smooth'] = savgol_filter(clean_xcom, window_length=11, polyorder=2)
 
@@ -35,10 +34,11 @@ plt.plot(
     linewidth=2
 )
 
-# Optional: Plot faint raw data points behind smoothed lines to visually compare
-plt.plot(df['timecode'], df['cop2bos_dist_cm'], color='#1f77b4', alpha=0.25, linestyle='--', label='COP (Raw)')
-plt.plot(df['timecode'], df['xcom2bos_dist_cm'], color='#ff7f0e', alpha=0.25, linestyle='--', label='XCoM (Raw)')
+# Semi transparent raw data points behind smoothed lines to visually compare
+# plt.plot(df['timecode'], df['cop2bos_dist_cm'], color='#1f77b4', alpha=0.25, linestyle='--', label='COP (Raw)')
+# plt.plot(df['timecode'], df['xcom2bos_dist_cm'], color='#ff7f0e', alpha=0.25, linestyle='--', label='XCoM (Raw)')
 
+# Case A: CoM < XcoM < CoP < BoSmax: 
 plt.fill_between(
     df['timecode'],
     df['cop2bos_smooth'],
@@ -46,20 +46,32 @@ plt.fill_between(
     where=(df['xcom2bos_smooth'] >= df['cop2bos_smooth']),
     color='#2ca02c',  # Green
     alpha=0.3,         # Transparency (30%)
-    interpolate=True,  # Fills cleanly up to the intersection points
-    label='case A'
+    interpolate=True,  
+    label='case A: stable'
 )
 
-# YELLOW: where XCoM is below COP
+# Case B: CoM < CoP < XcoM < BoSmax
 plt.fill_between(
     df['timecode'],
     df['cop2bos_smooth'],
     df['xcom2bos_smooth'],
     where=(df['xcom2bos_smooth'] < df['cop2bos_smooth']),
-    color='#ffe119',  # Soft Yellow
+    color='#ffe119',  # Yellow
     alpha=0.35,        # Transparency (35%)
     interpolate=True,
-    label='case B'
+    label='case B: unstable'
+)
+
+# Case C: XcoM > BoSmax
+plt.fill_between(
+    df['timecode'],
+    df['cop2bos_smooth'],
+    df['xcom2bos_smooth'],
+    where=(df['xcom2bos_smooth'] < 0),
+    color='#ff2617',  # Red
+    alpha=0.35,        # Transparency (35%)
+    interpolate=True,
+    label='case C: fall iminent'
 )
 
 # 3. Customizations
