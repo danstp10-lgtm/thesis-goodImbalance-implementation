d18fc03e740e9e951fab9167d478432546839a37
daniel <danstp10@gmail.com>
2026-08-17 15:15:49 +0200

    extracted distance plotting as a function and added a 2nd plot for showing solution space exploration

--- plotDistances.py
+++ plotDistances.py
@@ -2,14 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.signal import savgol_filter  # Optional: for Savitzky-Golay filtering
 from numpy import load
+import numpy as np
+import ast
+from matplotlib.patches import Polygon
+from scipy.spatial import ConvexHull
 
-def show_distances(file_path):
-    df = pd.read_csv(file_path)
-    df.columns = df.columns.str.strip()
-
-    # start time at 0
-    df['timecode'] = df['timecode'] - df['timecode'].iloc[0]
-    plt.figure(figsize=(12, 6), dpi=100)
+def make_distance_plot(df,ax):
+    df['timecode'] = df['timecode'] - df['timecode'].iloc[0] # start time at 0
     clean_cop = df['cop2bos_dist_cm'].interpolate(method='linear').bfill().ffill()
     clean_xcom = df['xcom2bos_dist_cm'].interpolate(method='linear').bfill().ffill()
 
@@ -22,7 +21,7 @@ def show_distances(file_path):
     # Remove XCoM velow -5
     df['xcom2bos_smooth'] = df['xcom2bos_smooth'].clip(lower=-5)
 
-    plt.plot(
+    ax.plot(
         df['timecode'], 
         df['cop2bos_smooth'], 
         label='COP to BOS (Smoothed)', 
@@ -30,7 +29,7 @@ def show_distances(file_path):
         linewidth=2
     )
 
-    plt.plot(
+    ax.plot(
         df['timecode'], 
         df['xcom2bos_smooth'], 
         label='XCoM to BOS (Smoothed)', 
@@ -39,11 +38,11 @@ def show_distances(file_path):
     )
 
     # Semi transparent raw data points behind smoothed lines to visually compare
-    # plt.plot(df['timecode'], df['cop2bos_dist_cm'], color='#1f77b4', alpha=0.25, linestyle='--', label='COP (Raw)')
-    # plt.plot(df['timecode'], df['xcom2bos_dist_cm'], color='#ff7f0e', alpha=0.25, linestyle='--', label='XCoM (Raw)')
+    # ax.plot(df['timecode'], df['cop2bos_dist_cm'], color='#1f77b4', alpha=0.25, linestyle='--', label='COP (Raw)')
+    # ax.plot(df['timecode'], df['xcom2bos_dist_cm'], color='#ff7f0e', alpha=0.25, linestyle='--', label='XCoM (Raw)')
 
     # Case A: CoM < XcoM < CoP < BoSmax: 
-    plt.fill_between(
+    ax.fill_between(
         df['timecode'],
         df['cop2bos_smooth'],
         df['xcom2bos_smooth'],
@@ -55,7 +54,7 @@ def show_distances(file_path):
     )
 
     # Case B: CoM < CoP < XcoM < BoSmax
-    plt.fill_between(
+    ax.fill_between(
         df['timecode'],
         df['cop2bos_smooth'],
         df['xcom2bos_smooth'],
@@ -67,7 +66,7 @@ def show_distances(file_path):
     )
 
     # Case C: XcoM > BoSmax
-    plt.fill_between(
+    ax.fill_between(
         df['timecode'],
         df['cop2bos_smooth'],
         df['xcom2bos_smooth'],
@@ -77,22 +76,42 @@ def show_distances(file_path):
         interpolate=True,
         label='case C: fall iminent'
     )
+    
+    ax.set_title('COP & XCoM Distance to Base of Support (Smoothed)')
+    ax.set_xlabel('Timecode (seconds)')
+    ax.set_ylabel('Distance (cm)')
+    ax.grid(True, linestyle='--', alpha=0.5)
+    ax.legend(loc='upper right', frameon=True, fontsize=10)
+    ax.ticklabel_format(useOffset=False, style='plain', axis='x')
 
-    plt.title('COP & XCoM Distance to Base of Support (Smoothed)', fontsize=14, fontweight='bold', pad=12)
-    plt.xlabel('Timecode (seconds)', fontsize=12)
-    plt.ylabel('Distance (cm)', fontsize=12)
-    plt.grid(True, linestyle='--', alpha=0.5)
-    plt.legend(loc='upper right', frameon=True, fontsize=10)
-    plt.ticklabel_format(useOffset=False, style='plain', axis='x')
+def make_CoM_path_plot(df,ax):
+    com_x = df["com_x"].interpolate(method='linear').bfill().ffill()
+    com_y = df["com_y"].interpolate(method='linear').bfill().ffill()
+    points = np.column_stack((com_x, com_y))
+    hull = ConvexHull(points)
 
-    plt.tight_layout()
-    plt.show()
+    # plot points and convex hull
+    ax.scatter(com_x, com_y, c='#37ed4c', label=f'CoM', alpha=0.7, edgecolors='k')
+    for simplex in hull.simplices:
+        ax.plot(points[simplex, 0], points[simplex, 1], 'r--', alpha=0.8)
+    ax.fill(points[hull.vertices, 0], points[hull.vertices, 1], 'red', alpha=0.15, label=f'Convex Hull Area: {hull.volume:.2f} cm^2')
 
-def show_CoP_path(frame_path):
-    pass
+    ax.set_title('CoM within BoS')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_xlim([0, 43])   # Replace with your desired [xmin, xmax]
+    ax.set_ylim([0, 17])
+    ax.invert_yaxis()
+    ax.legend(loc='upper right', frameon=True, fontsize=10)
+    ax.grid(True, linestyle=':', alpha=0.6)
 
+file_path = "recordings\session_20260817_142647\session_metrics.csv"
+# frames_path = "recordings\session_20260814_104425\\frames"
+df = pd.read_csv(file_path)
+df.columns = df.columns.str.strip()
 
-file_path = "recordings\session_20260814_104425\session_metrics.csv"
-frames_path = "recordings\session_20260814_104425\\frames"
-show_distances(file_path)
-show_CoP_path(frames_path)
+fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+make_distance_plot(df,axes[0])
+make_CoM_path_plot(df,axes[1])
+plt.tight_layout()
+plt.show()
