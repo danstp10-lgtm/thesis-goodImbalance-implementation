5ebd5a844578a1413301be65b5ef34a631a0d8db
daniel <danstp10@gmail.com>
2026-08-21 11:05:35 +0200

    adapted the main script to use built-in vive integration rather than custom solution

--- determineBalance.py
+++ determineBalance.py
@@ -4,7 +4,6 @@ from FileSaver import FileSaver
 import time
 from support_functions import *
 import cv2
-from triad_openvr import triad_openvr
 import numpy as np
 from collections import deque
 
@@ -16,13 +15,6 @@ if __name__ == "__main__":
     TSP_R = TSPDecoder(port="COM11",rows=rows, columns=columns) 
     MVN = XsensUDPListener(host="127.0.0.1", port=9764) # start Xsens listener
     saver = FileSaver(output_dir="recordings",frames_subdir="frames") # initialize file saver 
-    # Setup SteamVR listening
-    v = triad_openvr.triad_openvr()
-    # v.print_discovered_objects()
-    if "tracker_1" in v.devices:
-        v.rename_device("tracker_1","body_tracker")
-    if "tracker_2" in v.devices:
-        v.rename_device("tracker_2","TSP_corner")
     cached_raw_frame = np.zeros((rows, columns * 2 + between_patch_distance), dtype=np.uint8)
     # Visualization
     display_frame = np.zeros(cached_raw_frame.shape, np.uint8)
@@ -30,20 +22,7 @@ if __name__ == "__main__":
     # XCoM variables
     xsens_XCoM = None
     com_history = []
-    # Calibration parameters
-    frame_counter = 0
-    calibrated = False
-    calibration_samples = 300
-    sample_stride = 1
-    ALPHA = 0.01 
-    tundra_samples = []
-    xsens_calibration_samples = []
-    # Swap X and Y if patches are rotated
-    M_swap = np.array([
-        [0, 1, 0],  # X -> Y
-        [1, 0, 0],  # Y -> X
-        [0, 0, 1]   # Z -> Z
-    ])
+    calibrated = True
 
     print("Starting sync between Touch Sense Patch 1|2 - 7Hz and Xsens MVN Software - 240Hz")
     try:
@@ -51,77 +30,57 @@ if __name__ == "__main__":
             if MVN.new_data_available: # On new Xsens data
                 xsens_data = MVN.get_latest_data()
                 latest_xsens_CoM = xsens_data["com"] 
-                time_sec = xsens_data["timecode"] / 1000.0
+                time_sec = xsens_data["timecode"] 
                 xsens_segments = xsens_data["segments"]
-                body_tracker_coords = v.devices["body_tracker"].get_pose_quaternion()[0:3] # Tundra tracker
+                TSP_corner = xsens_data["tsp_corner"]
                 com_history.append((time_sec, latest_xsens_CoM))
 
-                # Determine transformation parameters: R, t
-                if not calibrated:     
-                    if len(xsens_calibration_samples) == calibration_samples: # Collect n samples of one Tundra and one Xsens tracker, at roughly the same place
-                        xsens_mat = np.array(xsens_calibration_samples)
-                        tundra_mat = np.array(tundra_samples)
-                        R, t, sim_error = get_Xsens2Tundra_transforms(xsens_mat, tundra_mat)
-                        print(f"Calibration complete similarity error:{sim_error} \n R: {R} \n t: {t} ")
-                        calibrated = True
-                    elif frame_counter % sample_stride == 0 and body_tracker_coords:
-                        xsens_calibration_samples.append(xsens_segments[0])
-                        tundra_samples.append(body_tracker_coords)
-                    frame_counter += 1    
-                else:
-                    # Compensate for drift overtime by applying R,t and comparing to actual tracker on the body 
-                    xsens2tundra_segments = ((R @ xsens_segments[0]) + t)        
-                    drift_error = body_tracker_coords - xsens2tundra_segments
-                    t += ALPHA * drift_error
+                if TSP_L.frame_available and TSP_R.frame_available: # on TSP available frame compute variables
+                    # Raw TSP data
+                    raw_frame_L, raw_frame_R = get_raw_frames(TSP_L,TSP_R)
+                    display_frame = cv2.cvtColor(cached_raw_frame, cv2.COLOR_GRAY2BGR)                  
+                    padding = np.zeros((rows,between_patch_distance)) # add empty space between hands
+                    cached_raw_frame = np.concatenate([raw_frame_L, padding,raw_frame_R], axis=1).astype(np.uint8) 
 
-                    if TSP_L.frame_available and TSP_R.frame_available: # on TSP available frame compute variables
-                        # Raw TSP data
-                        raw_frame_L, raw_frame_R = get_raw_frames(TSP_L,TSP_R)
-                        display_frame = cv2.cvtColor(cached_raw_frame, cv2.COLOR_GRAY2BGR)                  
-                        padding = np.zeros((rows,between_patch_distance)) # add empty space between hands
-                        cached_raw_frame = np.concatenate([raw_frame_L, padding,raw_frame_R], axis=1).astype(np.uint8) 
+                    # Calculate and translate XCoM, with smoothing
+                    t_TSP, R_TSP = TSP_corner[0], TSP_corner[1]
+                    com_aggragate = aggragate([item[1] for item in com_history][0])
+                    # print(f"aggregated {len(com_history)} frames from {com_history[0][0]} - {com_history[-1][0]} into {com_aggragate}")
+                    TSP_XCoM, TSP_CoM = process_XCoM(com_aggragate, time_sec, R_TSP, t_TSP, display_frame)
+                    com_history.clear() # clear history for next timestep                  
+                    
+                    # Check transformation with left hand
+                    xsens2TSP_segments = transform_Xsens2TSP(xsens_segments, R_TSP, t_TSP) * 100
+                    xsens2TSP_segments_pixel = [int(np.round(xsens2TSP_segments[0]/0.8)),int(np.round(xsens2TSP_segments[1]/0.6))] 
+                    print(f"test coords {xsens2TSP_segments_pixel} in shape {display_frame.shape}")
+                    if 0 < xsens2TSP_segments_pixel[0] < display_frame.shape[1] and 0 < xsens2TSP_segments_pixel[1] < display_frame.shape[0]:
+                        cv2.drawMarker(
+                            display_frame,
+                            (xsens2TSP_segments_pixel[0], xsens2TSP_segments_pixel[1]),
+                            color=COLOR_HAND,
+                            markerType=cv2.MARKER_CROSS,
+                            markerSize=1,
+                            thickness=1,
+                        )
+                    
+                    # Calculate TSP metrics
+                    CoP_cm = process_CoP(cached_raw_frame, display_frame)
+                    BoS_cm = process_BoS(cached_raw_frame, display_frame)
+                    
+                    # Calculate minimum distance of CoP and XCoM to BoS boundaries in cm, margin of stability b
+                    min_dist_CoP2BoS, min_dist_XCoM2BoS, min_dist_CoM2BoS = calculate_min_dist(BoS_cm, CoP_cm, TSP_CoM, TSP_XCoM)
 
-                        # Calculate XCoM, with smoothing
-                        TSP_corner = np.array(v.devices["TSP_corner"].get_pose_matrix().m) # get tundra pose matrix
-                        t_TSP = TSP_corner[0:3, 3] 
-                        R_TSP = TSP_corner[0:3, 0:3] 
-                        com_aggragate = aggragate([item[1] for item in com_history][0])
-                        # print(f"aggregated {len(com_history)} frames from {com_history[0][0]} - {com_history[-1][0]} into {com_aggragate}")
-                        TSP_XCoM, TSP_CoM = process_XCoM(com_aggragate, time_sec, R, t, R_TSP, t_TSP, display_frame)
-                        com_history.clear() # clear history for next timestep                  
-                        
-                        # Check transformation with left hand
-                        xsens2TSP_segments = transform_Xsens2TSP(xsens_segments[0],R, t, R_TSP, t_TSP) * 100
-                        xsens2TSP_segments_pixel = [int(np.round(xsens2TSP_segments[0]/0.8)),int(np.round(xsens2TSP_segments[1]/0.6))] 
-                        print(f"test coords {xsens2TSP_segments_pixel} in shape {display_frame.shape}")
-                        if 0 < xsens2TSP_segments_pixel[0] < display_frame.shape[1] and 0 < xsens2TSP_segments_pixel[1] < display_frame.shape[0]:
-                            cv2.drawMarker(
-                                display_frame,
-                                (xsens2TSP_segments_pixel[0], xsens2TSP_segments_pixel[1]),
-                                color=COLOR_HAND,
-                                markerType=cv2.MARKER_CROSS,
-                                markerSize=1,
-                                thickness=1,
-                            )
-                        
-                        # Calculate TSP metrics
-                        CoP_cm = process_CoP(cached_raw_frame, display_frame)
-                        BoS_cm = process_BoS(cached_raw_frame, display_frame)
-                        
-                        # Calculate minimum distance of CoP and XCoM to BoS boundaries in cm, margin of stability b
-                        min_dist_CoP2BoS, min_dist_XCoM2BoS, min_dist_CoM2BoS = calculate_min_dist(BoS_cm, CoP_cm, TSP_CoM, TSP_XCoM)
+                    # Save data
+                    saver.save_frame(cached_raw_frame, time_sec)
+                    if min_dist_CoM2BoS > 0:
+                        CoM_in_BoS = TSP_CoM
+                    else:
+                        CoM_in_BoS = None
+                    saver.save_metrics(time_sec, CoP_cm, min_dist_CoP2BoS, CoM_in_BoS, TSP_XCoM, min_dist_XCoM2BoS, BoS_cm)
+                    saver.increment_frame_count()
 
-                        # Save data
-                        saver.save_frame(cached_raw_frame, time_sec)
-                        if min_dist_CoM2BoS > 0:
-                            CoM_in_BoS = TSP_CoM
-                        else:
-                            CoM_in_BoS = None
-                        saver.save_metrics(time_sec, CoP_cm, min_dist_CoP2BoS, CoM_in_BoS, TSP_XCoM, min_dist_XCoM2BoS, BoS_cm)
-                        saver.increment_frame_count()
-
-                        # Output synchronized packet info
-                        # print(f"Time: {time_sec}s | TSP_XCoM: {TSP_XCoM} | CoP (cm): {CoP_cm} | MinDistCoP: {min_dist_CoP2BoS} | MinDistXCoM {min_dist_XCoM2BoS}")
+                    # Output synchronized packet info
+                    # print(f"Time: {time_sec}s | TSP_XCoM: {TSP_XCoM} | CoP (cm): {CoP_cm} | MinDistCoP: {min_dist_CoP2BoS} | MinDistXCoM {min_dist_XCoM2BoS}")
             else:
                 time.sleep(0.0005)  # Yield CPU to UDP thread
             
